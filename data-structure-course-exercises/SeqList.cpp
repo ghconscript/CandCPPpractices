e2c0400b409e51efd0e604b5cb962fc73196f95a
ggmu <ghconscript@163.com>
2025-09-10 22:55:39 +0800

    finish two function

--- data-structure-course-exercises/SeqList.cpp
+++ data-structure-course-exercises/SeqList.cpp
@@ -2,12 +2,35 @@
 #define MAX 100
 using namespace std;
 struct List{
-	int elements[max] // ElemType == int 
+	int elements[MAX];// ElemType == int 
 	int last;
 };
-void Delete(&List);
-void DeleteRepeatElements(&List);
-void Reverse(&List);
+void Delete(struct List& L, int target){
+	int i;
+	for (i=target;i<L.last;i++) {
+		L.elements[i]=L.elements[i+1];	
+	}
+	L.last-=1;
+}
+void DeleteRepeatElements(struct List&L){
+	int i,j=1;
+	for(i=1;i<=Last;i++){
+		if(L.elements[i]!=L.elements[i-1]){
+			L.elements[j]=L.elements[i];
+			j++;
+		}
+		L.last=j;
+	} 
+}
+void Reverse(struct List &L){
+	int i;
+	int j;
+	for (i=0;i<=L.Last/2;i++){
+		j=L.elements[i];
+		L.elements[i]=L.elements[L.last-i];
+		L.elements[L.last-i]=j;
+	}
+}
 void LeftForward(&List,int k);
 void RightForward(&List int k);
 void combine(&List);
