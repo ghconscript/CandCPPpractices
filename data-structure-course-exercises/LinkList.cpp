0dbf244d88dbae61ebc09116fde6fe665f7c4ca1
ggmu <ghconscript@163.com>
2025-09-13 15:52:03 +0800

    finish the LinkList

--- data-structure-course-exercises/LinkList.cpp
+++ data-structure-course-exercises/LinkList.cpp
@@ -50,6 +50,83 @@ void Reverse(LIST &head){
 	}
 	head=prev;//循环结束更新头指针 
 }
+void LeftForward(LIST &head,int k){
+	/*
+	int i;
+	position last = head->next;//存储移动完之后的last
+	position final = head->next;
+	while(final->next!=NULL){//存储原来的最后一位 
+		final=final->next;
+	} 
+	
+	for(i=1;i<k;i++){
+		last=last->next;//找到了last的位置 
+	}final->next=head->next;
+	head->next=last->next;
+	last->next=NULL;
+	
+	*/
+	//更好的写法  也可以转化为循环链表 
+	int n;
+	position tail=head->next;
+	position new_tail=head->next;
+	while(tail->next!=NULL){
+		n++;
+		tail=tail->next;
+	}
+	k=k%n;
+	for (i=1;i<k;i++){
+		new_tail=new_tail->next;
+	}
+	tail->next=head->next;
+	head->next=new_tail->next;
+	new_tail->next=NULL;
+	
+}
+void RightForward(LIST &head,int k){
+	int n;
+	position tail=head->next;
+	position new_tail=head->next;
+	while(tail->next!=NULL){
+		n++;
+		tail=tail->next;
+	}
+	k=k%n;
+	for (i=1;i<n-k;i++){
+		new_tail=new_tail->next; 
+	}
+	tail->next=head->next;
+	head->next=new_tail->next;
+	new_tail->next=NULL;
+}
+void INSERT (position p1,position p2){//辅助 将p2插入p1后 
+	p2->next=p1->next;
+	p1->next=p2; 
+}
+void merge (LIST &L1,LIST &L2){
+	//假设降序 并后降序
+	position p=L1->next;
+	position prev=L1;
+	position q=L2->next;
+	position temp=NULL;
+	while(p!=NULL&&q!NULL){
+		if(p->data<=q->data){
+			temp=q->next;
+			INSERT(prev,q);
+			prev=q;
+			q=temp;
+		}else{
+			prev=p;
+			p=p->next;
+		}
+	}
+	if(q!=NULL){//while 循环结束后 p是NULL 所以是prev->next 等于q 
+		prev->next=q;	
+	}
+	L2->next=NULL;
+}
+
 int main() {
+	
 	
 }
