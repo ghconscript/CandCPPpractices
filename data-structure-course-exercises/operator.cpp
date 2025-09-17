814a78b5edb02baeacf96ea1bdd4965879c7ce0d
ggmu <ghconscript@163.com>
2025-09-17 23:06:00 +0800

    wrong result of operating ,tomorrow continue finishing it.

--- data-structure-course-exercises/operator.cpp
+++ data-structure-course-exercises/operator.cpp
@@ -4,7 +4,7 @@
 #define MAX 100
 using namespace std;
 typedef struct{
-	char elements[MAX];// elemtype == char
+	int elements[MAX];// elemtype == char
 	int top;//顶值对应的索引 
 }STACK;
 void InitStack(STACK &S){
@@ -53,40 +53,75 @@ int main(){
 	STACK stack;
 	InitStack(stack);
 	getline(inFile, mid);
-	cout << mid << endl;
+	cout <<"中缀表达式"<< mid << endl;
 	int i=0;
 	char c=mid[i];
 	while (c!='\0'){
-		
+		//对于c++风格字符串 可以不依赖/0 
 		if(c>='0'&&c<='9'){
-			back += c;//相当于直接输出 
+			back += c;//直接输出 
 			
 		}else if(c=='('){
-			Push(c,stack);
+			Push(c,stack);//左括号入栈 
 			
 		}else if(c==')'){
 			while(stack.elements[stack.top]!='('){
 				back += stack.elements[stack.top];
-				Pop(stack);
+				Pop(stack);//不断弹出符号 直到遇到左括号 
 			}
-			Pop(stack);//弹出左括号 
+			Pop(stack);//弹出左括号  但不加入back 
 			
 		}else{
 			while(Priority(c)<=Priority(stack.elements[stack.top])){
 				back += stack.elements[stack.top];
-				Pop(stack);
+				Pop(stack);//如果栈顶符号优先级大于等于c 将栈顶运算符弹出输出 不断比较新的运算符直到入栈   
 			}
-			Push(c,stack);
+			Push(c,stack);//入栈 
 		}
-		i++;
+		i++;//遍历下一个元素 
 		c=mid[i];
 		
 	}
 		while(!Empty(stack)){
 			back +=stack.elements[stack.top];
-			Pop(stack);
+			Pop(stack);//若栈非空的话 不断弹出剩余的栈内符号 
 		}
-	
 	cout<<back<<endl;
+	i=0;
+	int temp1,temp2,result;
+	char cresult;
+	c=back[i];
+	
+	//计算模块 
+	while(i<back.size()){
+		
+		if(c>='0'&&c<='9'){
+			Push(c,stack);//数字直接入栈 
+		}else {// 符号 栈顶两个元素运算并将结果压入栈中 
+			temp1=int(stack.elements[stack.top])-'0';
+			Pop(stack);
+			temp2=int(stack.elements[stack.top])-'0';
+			Pop(stack);
+			switch(c){
+				case '+':
+					result = temp2+temp1;
+					break;
+				case '-':
+					result = temp2-temp1;
+					break;
+				case '*':
+					result = temp1*temp2;
+					break;
+				case '/':
+					result = temp2/temp1;
+					break;
+			}
+			cresult=result+'0';
+			Push(cresult,stack); 
+		}
+		i++;//back的下一个元素 
+		c=back[i];
+	}
+	cout<<result<<endl;
 }
 
