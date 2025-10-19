62cdb90e3a64e566321f374bfbd0a258e0a60991
ggmu <ghconscript@163.com>
2025-10-19 23:11:57 +0800

    finish the datastruct but not finish the func

--- datastructure/course/Huffmantree.cpp
+++ datastructure/course/Huffmantree.cpp
@@ -1,31 +1,75 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#define NUM 52
 using namespace std;
-typedef struct{
+//把所有节点依次建立 然后建立一个最小堆 建立最小堆后 依次把最小堆堆顶元素提取出来作为Huffman树的最小节点
+typedef struct {
 	double weight;
 	int lchild;
 	int rchild;
 	int parent;
-}HTNODE;
-typedef HTNODE HuffmanT[51];//表示 “一个包含 51 个HTNODE元素的数组”。
-void InitHT(HuffmanT){
-	for(int i=0;i<=0;i++){
-		//TODO
+} HTNODE;
+typedef struct {
+	HTNODE data[NUM];
+	int n;//有效节点的数量
+} MinHeap;
+typedef HTNODE HuffmanT[NUM];//表示 “一个包含 51 个HTNODE元素的数组”。
+void InitHP(MinHeap M) {
+	M.n = 0;
+}
+void Insert(MinHeap&heap, HTNODE N) {
+	int i;
+	if (heap.n >= 52) {
+		cout << "full" << endl;
+		return;
+	} else {
+		i = heap.n + 1;
+		while ((i != 1) && (N.weight < heap.data[2 / i].weight)) { //非空且 N比父节点小
+			heap.data[i] = heap.data[2 / i]; //把上面的节点下移一个
+			i /= 2;
+		}
+		heap.data[i] = N;//将N插入指定位置
 	}
 }
-void CreatHT(HuffmanT T){
-	int i,p1,p2;
+void InitHT(HuffmanT T) {
+	for (int i = 0; i <= NUM - 1; i++) {
+		T[i] = {0, -1, -1, -1};
+	}
+}
+void InputW(HuffmanT T, int cnt[]) {
+	for (int i = 0; i <= NUM - 1; i++) {
+		T[i].weight = cnt[i];
+	}
+}
+void CreatHT(HuffmanT T, int cnt[]) {
+	int i, p1, p2;
+	InitHT(T);
+	InputW(T, cnt);
+}
+HTNODE DeleteMax(MinHeap &heap){//见课件的P121
+	int parent =1 ,child=2;
+	HTNODE elem,tmp;
+	if(heap.n!=0){
+		elem = heap.data[1];
+		tmp=heap.data[heap.n--];
+		while(child<=heap.n){
+			
+		}
+		
+	}
 	
 }
-int main(){
+void SelectMin(HuffmanT T){
+	
+}
+int main() {
 	ifstream inFile;
-	int cnt[26]={0};
-	int Cnt[26]={0};
-	int total=0;
+	int cnt[52] = {0};
+	int total = 0;
 	inFile.open("data.txt");
 	char c;
-	while(inFile.get(c)){
+	while (inFile.get(c)) {
 		if (c >= 'a' && c <= 'z') {
 			// 计算对应下标（例如 'a' - 'a' = 0，'b'-'a'=1 ...）
 			int index = c - 'a';
@@ -34,10 +78,10 @@ int main(){
 		}
 		if (c >= 'A' && c <= 'Z') {
 			// 计算对应下标（例如 'a' - 'a' = 0，'b'-'a'=1 ...）
-			int index = c - 'A';
-			Cnt[index]++;  // 对应字母计数+1
+			int index = c - 'A' + 26;
+			cnt[index]++;  // 对应字母计数+1
 			total++;
 		}
 	}
-	
+
 }
